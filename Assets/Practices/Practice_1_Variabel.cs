a7fe8fddfd6d37d6a511778807074fa1c21b0fd3
wiejerry <mr971003jerry@gmail.com>
2025-01-18 13:59:30 +0800

    Update 完成作業練習

---
+++ Assets/Practices/Practice_1_Variabel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Practice_1_Variabel : MonoBehaviour 
+{
+    public int price = 999;
+    private float jumpHeight = 77.5f;
+    public string weapon = "三項之力";
+    private bool getDragonSoul = true;
+}
---
+++ Assets/Practices/practice_2_DateType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class practice_2_DateType : MonoBehaviour 
+{
+    public float variadleFloat = 7.7f;
+    public double variadleDouble = 0.1235;
+    public decimal variadleDecimal = 1.23456789m;
+}
