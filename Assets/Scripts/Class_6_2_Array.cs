d0a688d129f5479c765d55ba44b53ce0e7648e8b
wiejerry <mr971003jerry@gmail.com>
2025-02-10 19:56:45 +0800

    Update 認識不規則陣列

--- Assets/Scripts/Class_6_2_Array.cs
+++ Assets/Scripts/Class_6_2_Array.cs
@@ -7,6 +7,7 @@ namespace Jerry
     /// </summary>
     public class Class_6_2_Array : MonoBehaviour
     {
+        #region 一到三微陣列
         // 不使用陣列的寫法 :
         // 皮卡丘、小火龍、卡比獸
         public string card1 = "皮卡丘", card2 = "小火龍", card3 = "卡比獸";
@@ -32,6 +33,11 @@ namespace Jerry
             // 第二頁
             { {"精靈球","高級球"}, {"大師球","巢穴球"} }
         };
+        #endregion
+
+        private int[][] numbers = new int[2][];
+
+        private int[][,] count = new int[2][,];
 
         private void Start()
         {
@@ -47,7 +53,7 @@ namespace Jerry
             // Sat 設定陣列的資料
             // 陣列名稱[編號] 指定 值;
             // 將傑尼龜換成傑水箭龜
-            deck2[2] = "水箭龜";            
+            deck2[2] = "水箭龜";
             Debug.Log($"<color=#f39>Deck2 的第三張卡片 : {deck2[2]}</color>");
             #endregion
 
@@ -67,6 +73,7 @@ namespace Jerry
             Debug.Log($"<color=#f3d>第二頁第一排第二個 : {shop[1, 0, 1]}</color>");
             #endregion
 
+            #region 陣列長度與維度
             // 獲得陣列長度或維度
             // 一維 ~ 多維陣列的長度 : 陣列名稱.Length
             Debug.Log($"<color=#f93>一維 - 排組 2 的長度 : {deck2.Length}</color>");
@@ -76,6 +83,23 @@ namespace Jerry
             Debug.Log($"<color=#f93>一維 - 排組 2 的維度 : {deck2.Rank}</color>");
             Debug.Log($"<color=#f93>二維 - 道具的維度 : {invenyory.Rank}</color>");
             Debug.Log($"<color=#f93>三維 - 商品的維度 : {shop.Rank}</color>");
+            #endregion
+
+            numbers[0] = new int[] { 1, 3, 5 };
+            numbers[1] = new int[] { 9, 8 };
+
+            // 取得數字 3 與 9
+            Debug.Log($"<color=#3ff>不規則陣列的數字 3 : {numbers[0][1]}");
+            Debug.Log($"<color=#3ff>不規則陣列的數字 9 : {numbers[1][0]}");
+
+            // 將 8 改成 6
+            numbers[1][1] = 6;
+            Debug.Log($"<color=#3ff>第二列的第二個 : {numbers[1][1]}");
+
+            // 不規則陣列 : 包含多維陣列
+            count[0] = new int[,] { { 1, 1 }, { 1, 1 } };
+            count[1] = new int[,] { { 2, 2, 2 }, { 2, 2, 2 } };
+            Debug.Log($"<color=#3ff>count 第二陣列的[0, 0] : {count[1][0, 0]}");
         }
     }
 }
