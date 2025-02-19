d41661a238db774d93e834d742838d55d0ddf192
wiejerry <mr971003jerry@gmail.com>
2025-02-19 20:49:15 +0800

    Update  資料類型轉換

--- Assets/Tools/LogSystem.cs
+++ Assets/Tools/LogSystem.cs
@@ -20,5 +20,18 @@ namespace Jerry.Tools
             return result ;
         }
 
+
+        /// <summary>
+        /// 輸出訊息並指定顏色
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        /// <returns>包含顏色的訊息</returns>
+        public static string LogWithColor(object message, string color)
+        {
+            string result = $"<color={color}>{message}</color>";
+            Debug.Log(result);
+            return result;
+        }
     }
 }
